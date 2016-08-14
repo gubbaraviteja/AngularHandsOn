c567cca8bac809c65d97a7c7fa29def8de151163
gubbaraviteja <gubbaraviteja@gmail.com>
2016-08-14 11:08:48 +0200

    Tried connecting to firebase

--- angularHandsOn/WebContent/app.js
+++ angularHandsOn/WebContent/app.js
@@ -1,3 +1,20 @@
-(function(){
-	var app = angular.module('plunker', []);
+(function() {
+
+	var config = {
+		apiKey : "AIzaSyCT20vdeQM_c-szUoBaLYmp1nn9IVIsNQQ",
+		authDomain : "learnangular-ff01c.firebaseapp.com",
+		databaseURL : "http://learnangular-ff01c.firebaseio.com",
+		storageBucket : "learnangular-ff01c.appspot.com",
+	};
+	firebase.initializeApp(config);
+
+	var plunker = angular.module('plunker', [ 'firebase' ]).controller('MyCtrl',
+			function($firebaseObject) {
+				const
+				rootRef = firebase.database().ref().child('angular');
+				const
+				ref = rootRef.child('object');
+				this.object = $firebaseObject(ref);
+			});
+
 })();
